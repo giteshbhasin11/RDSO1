4446dc97d0355d971439f183b0c26a7942f6926d
DivishaBedi <divisha.bedi@gmail.com>
2017-06-21 15:24:21 +0530

    kuch bhi

---
+++ app/src/main/java/com/example/divisha/orhreservation/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.divisha.orhreservation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView fp = (TextView) findViewById(R.id.fp);
+        fp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent i = new Intent(getApplicationContext(),forgtpswrd.class);
+                startActivity(i);
+            }
+        });
+    }
+}
