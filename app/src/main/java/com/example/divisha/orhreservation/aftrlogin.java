8da4ab941ec2cbac2d2aaed9ff431b6ce9cf38fd
DivishaBedi <divisha.bedi@gmail.com>
2017-06-29 02:48:22 +0530

    progress

--- app/src/main/java/com/example/divisha/orhreservation/aftrlogin.java
+++ app/src/main/java/com/example/divisha/orhreservation/aftrlogin.java
@@ -1,7 +1,10 @@
 package com.example.divisha.orhreservation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class aftrlogin extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class aftrlogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aftrlogin);
+
+        TextView edit = (TextView)findViewById(R.id.edit);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),editinfo.class);
+                startActivity(i);
+            }
+        });
     }
 }
